8724f2ff9e36eab17da3c722ffae050cfd118bd7
Johannes Brauer <brauer@nordakademie.de>
2018-06-11 22:02:50 +0200

    Zwischenversion

--- nonopolyful/project.clj
+++ nonopolyful/project.clj
@@ -3,14 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [compojure "1.5.1"]
                  [ring/ring-defaults "0.2.1"]
                  [ring/ring-json "0.3.1"]
                  [org.clojure/clojurescript "1.10.238"]
                  [cljs-http "0.1.45"]
                  [org.clojure/core.async "0.4.474"]
-                 [enfocus "2.1.1"]]
+                 [enfocus "2.1.1"]
+                 [nonopolyful "0.1.0-SNAPSHOT"]]
   :plugins [[lein-ring "0.9.7"]
             [lein-cljsbuild "1.1.1"]]
   :ring {:handler nonopolyful.handler/app}
--- nonopolyful/src/nonopolyful/tracing.clj
+++
@@ -1,6 +0,0 @@
-(ns nonopolyful.tracing)
-
-(def TRACE true)
-(defn move-trace [& args]
-  (when TRACE (do (print ">>>Trace>>> ") (apply println args))))
-
